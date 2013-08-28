e09c95ec295275acea82b252758825a90dadc923
guidopujadas@gmail.com <guidopujadas@gmail.com@4b19323d-f289-0fe8-b038-d169f439b240>
2013-08-28 05:33:21 +0000

    tests

---
+++ trunk/VirtualDT/src/test/java/grupo3/tests/TestUsuario.java
@@ -0,0 +1,37 @@
+package grupo3.tests;
+
+import grupo3.Usuario;
+import grupo3.exception.ValidaciónException;
+
+import java.util.Date;
+import java.util.GregorianCalendar;
+
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class TestUsuario {
+	Usuario usuario;
+	Usuario usuarioYaValidado;
+	final String CodigoDeValidacion = "3e4r5t";
+	
+	@Before
+	public void miSetUp(){
+		Date fechaNac = new GregorianCalendar(2000,02,01).getTime();
+		usuario = new Usuario("username", "password", "nombre", "apellido", "email",fechaNac);
+		usuarioYaValidado = new Usuario("username1", "password1", "nombre1", "apellido1", "email1",fechaNac);
+		usuarioYaValidado.validarCuenta(CodigoDeValidacion);
+	}
+	
+	@Test
+	public void testValidarCuentaNoExistente(){
+		usuario.validarCuenta(CodigoDeValidacion);
+		Assert.assertTrue(usuario.getEstaValidado());
+	}
+	
+	@Test(expected=ValidaciónException.class)
+	public void testValidarCuentaExistente(){
+		usuarioYaValidado.validarCuenta(CodigoDeValidacion);
+	}
+}
