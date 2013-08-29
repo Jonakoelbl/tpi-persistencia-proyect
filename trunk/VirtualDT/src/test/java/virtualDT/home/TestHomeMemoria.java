2dfed73377803f25022b93a65341e81f042f8bb2
guidopujadas@gmail.com <guidopujadas@gmail.com@4b19323d-f289-0fe8-b038-d169f439b240>
2013-08-29 14:39:41 +0000

    tests

--- trunk/VirtualDT/src/test/java/virtualDT/home/TestHomeMemoria.java
+++ trunk/VirtualDT/src/test/java/virtualDT/home/TestHomeMemoria.java
@@ -6,12 +6,16 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
+import virtualDT.exception.NuevaPasswordInvalida;
+import virtualDT.exception.UsuarioYaExisteException;
+import virtualDT.exception.ValidaciónException;
+
 public class TestHomeMemoria {
 	
 	HomeMemoria homeMem;
 	Usuario usuario;
 	Usuario usuarioYaValidado;
-
+    Usuario usuarioYaValidado1;
 	
 	@Before
 	public void miSetUp(){
@@ -21,6 +25,9 @@ public class TestHomeMemoria {
 		usuarioYaValidado = new Usuario("username1", "password1", "nombre1", "apellido1", "email1",fechaNac);
 		homeMem.getUsuariosRegistrados().add(usuario);
 		homeMem.registrarUsuario(usuarioYaValidado);
+		usuarioYaValidado1 = new Usuario("username1", "password1", "nombre1", "apellido1", "email1",fechaNac);
+		homeMem.getValidaciones().put("b12357",usuarioYaValidado1);
+		homeMem.validarCuenta("b12357");
 	}
 	
 	@Test
@@ -28,7 +35,7 @@ public class TestHomeMemoria {
 		Assert.assertTrue(homeMem.getUsuariosRegistrados().contains(usuario));
 	}
 	
-	@Test
+	@Test(expected = UsuarioYaExisteException.class)
 	public void testRegistrarUsuarioExistente(){
 		homeMem.registrarUsuario(usuarioYaValidado);
 	}
@@ -46,13 +53,25 @@ public class TestHomeMemoria {
 		Assert.assertEquals(usuario.getPassword(),"nuevaPassword");
 	}
 	
-	@Test
+	@Test(expected=NuevaPasswordInvalida.class)
 	public void testcambiarPasswordViejaIncorrecta(){
 		homeMem.cambiarPassword(usuario.getUsername(),"viejaPassword","nuevaPassword");
 	}
 	
-	@Test
+	@Test(expected=NuevaPasswordInvalida.class)
 	public void testcambiarPasswordNuevaVacia(){
 		homeMem.cambiarPassword(usuario.getUsername(),"viejaPassword","");
+	}
+	
+	////////////////////////////////////////////////////////////////////////////////
+	
+	@Test(expected=ValidaciónException.class)
+	public void testUsuarioYaValidado(){
+		homeMem.validarCuenta("b12357");
+		homeMem.validarCuenta("b123579");
+	}
+	
+	public void testUsuarioRegistradoEnHashmap(){
+		Assert.assertTrue(homeMem.getValidaciones().containsValue(usuarioYaValidado));
 	}
 }
