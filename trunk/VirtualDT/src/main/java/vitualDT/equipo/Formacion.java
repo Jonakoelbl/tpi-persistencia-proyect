ee12b0521578081233a7d7388ba1235bed53e48c
guidopujadas@gmail.com <guidopujadas@gmail.com@4b19323d-f289-0fe8-b038-d169f439b240>
2013-09-05 05:11:59 +0000

    partidos sin tests

--- trunk/VirtualDT/src/main/java/vitualDT/equipo/Formacion.java
+++ trunk/VirtualDT/src/main/java/vitualDT/equipo/Formacion.java
@@ -1,17 +1,43 @@
 package vitualDT.equipo;
 
-import java.util.List;
-import java.util.Vector;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Formacion {
-	private List<Jugador> jugadores = new Vector<Jugador>();
+	Map<Jugador,Posicion> jugadoresConPosiciones= new HashMap<Jugador, Posicion>();
 	
+	public Map<Jugador, Posicion> getJugadoresConPosiciones() {
+		return jugadoresConPosiciones;
+	}
+
+
+	public void setJugadoresConPosiciones(
+			Map<Jugador, Posicion> jugadoresConPosiciones) {
+		this.jugadoresConPosiciones = jugadoresConPosiciones;
+	}
+
+
 	public Formacion() { // Hibernate ...
 	}
 	
-	public Formacion(List<Jugador> jugadores){
+	
+	/*public Formacion(List<Jugador> jugadores){
 		this.jugadores.addAll(jugadores);
 	}
+
+	public List<Jugador> getJugadores() {
+		return jugadores;
+	}
+
+	public void setJugadores(List<Jugador> jugadores) {
+		this.jugadores = jugadores;
+	}*/
 	
-	//TODO calcular el puntaje
+	public int puntajeFormacion(){
+		int puntajeTotal = 0;
+		for(Jugador j : this.getJugadoresConPosiciones().keySet()){
+			puntajeTotal = puntajeTotal + j.puntajeDeJugadorEnPosicion(this.getJugadoresConPosiciones().get(j));
+		}
+		return puntajeTotal;
+	}
 }
