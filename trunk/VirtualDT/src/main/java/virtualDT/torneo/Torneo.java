5f552f8fe7ec4f746ba7445746105b4cdd212f85
Jonakoelbl <Jonakoelbl@4b19323d-f289-0fe8-b038-d169f439b240>
2013-09-26 18:15:45 +0000

    Refactorizacion del codigo : - La logica del modelo de los partidos haciendolo mas generico a la hora de realizar los dos tipos de torneos. - La logica de Partido paso a ser PartidoSimple. - Creacion del mapeo de Torneo, PartidoDeCopa y PartidoSimple.

--- trunk/VirtualDT/src/main/java/virtualDT/torneo/Torneo.java
+++ trunk/VirtualDT/src/main/java/virtualDT/torneo/Torneo.java
@@ -1,5 +1,36 @@
 package virtualDT.torneo;
 
-public abstract class Torneo {
-	public void jugarTorneo(){}
+import java.util.List;
+import java.util.Vector;
+
+import virtualDT.equipo.Equipo;
+import virtualDT.equipo.Partido;
+
+public abstract class Torneo<R extends TipoResultado, T extends Partido<R>> {
+	protected  List<T> partidos = new Vector<T>();
+	protected List<R> resultados = new Vector<R>();
+	
+	public void jugarTorneo() {
+		for (T partido : this.partidos) {
+			this.resultados.add(partido.jugarPartido());
+		}
+	}
+	
+	public abstract void armarPartidos(List<Equipo> equipos);
+
+	public List<T> getPartidos() {
+		return partidos;
+	}
+
+	public void setPartidos(List<T> partidos) {
+		this.partidos = partidos;
+	}
+
+	public List<R> getResultados() {
+		return resultados;
+	}
+
+	public void setResultados(List<R> resultados) {
+		this.resultados = resultados;
+	}
 }
